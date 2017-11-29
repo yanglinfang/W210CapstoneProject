7e5bd2dac9519da3e7f05eebac27b65e473603a1
yanglinfang <yanglinfang1226@gmail.com>
2017-11-29 14:58:16 -0800

    add more data files

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ from forms import SymbolSearch
 from patentSearchForm import PatentSearch
 import query_similarity
 
+import csv
+import os.path
+import word_cloud
+
 # login to plot.ly
 py.sign_in('yanglinfang', 'F5jaY29GDfD8TIenMz9p')
 
@@ -34,6 +38,7 @@ def main():
                                 trend2=request.form['trend2']))
     return render_template('selection.html', form=form)
 
+
 @app.route("/symbol/<symbol>+<trend1>+<trend2>")
 def results(symbol, trend1, trend2):
     data = web.DataReader(symbol, data_source='yahoo')
@@ -44,38 +49,45 @@ def results(symbol, trend1, trend2):
     return render_template('plotly.html', symbol=symbol,
                            plot=url, table=table)
 
+
 @app.route("/home", methods=['GET'])
 def home():
     return render_template('home.html')
+
 
 @app.route("/", methods=['GET'])
 @app.route("/patent", methods=['GET', 'POST'])
 def patentMain():
     form = PatentSearch(csrf_enabled=False)
     if request.method == 'POST' and form.validate():
-        # Load search result 
-        return redirect(url_for('patentResults', 
-                        abs=request.form['patent_abs'],
-                        sim=request.form['patent_cosine_sim_threshold'],
-                        top=request.form['patent_result_top']))
+        # Load search result
+        return redirect(url_for('patentResults',
+                                abs=request.form['patent_abs'],
+                                sim=request.form['patent_cosine_sim_threshold'],
+                                top=request.form['patent_result_top']))
     return render_template('patentSearch.html', form=form)
+
 
 @app.route("/patentResults/<abs>+<sim>+<top>")
 def patentResults(abs, sim, top):
-    # call from query_similarity to get search results 
+    # call from query_similarity to get search results
     data, data_type = query_similarity.search(abs, sim, top)
-    # convert dataframe to table, tell pandas to not truncate width 
+    # convert dataframe to table, tell pandas to not truncate width
     pd.set_option('display.max_colwidth', -1)
     table = data.to_html()
-    return render_template('patentSearchResults.html', abs=abs, sim=sim, top=top, table=table) 
+    return render_template('patentSearchResults.html', abs=abs, sim=sim, top=top, table=table)
+
 
 @app.route("/patent/wordcloud", methods=['GET'])
 def wordCloud():
-    return render_template('wordCloud.html')
+    fName = 'x0-0x'
+    filePath = "../" + word_cloud.createTitleSummaryFile(fName, 200)
+    return render_template('wordCloud.html', filePath=filePath)
+
 
 if __name__ == '__main__':
     #if running local, you can use the following line
     #app.run(host='0.0.0.0', port=7777, debug=True)
 
-    #if deploy to heroku, use the following line instead 
+    #if deploy to heroku, use the following line instead
     app.run()
--- word_cloud.py
+++ word_cloud.py
@@ -2,16 +2,17 @@ import csv
 import os.path
 
 allowedId = ['0','1','2','3','4','5','6','7','8','9','x']
-prefix = 'summary_'
+pathPrefix = 'static/data/topics/'
+prefix = pathPrefix + 'summary_'
 ending = '.csv'
 header = ['word', 'count']
 fileNameSplit = '-'
 def loadTitles(fileId, rowId):
-    fName = 'topics_'+ fileId + ending
+    fName = pathPrefix + 'topics_'+ fileId + ending
     returnList = [] 
     allRows = [] 
     if(fileId in allowedId):
-        with open(fName, newline='') as csvfile:
+        with open(fName) as csvfile:
             spamreader = csv.reader(csvfile, delimiter= ' ', quotechar='|')
             for row in spamreader:
                 #print(len(row))
@@ -33,15 +34,20 @@ def getDict(inputList):
         counts[i] = counts.get(i, 0) + 1
     return counts 
 
-def saveToCsv(fName, counts):
+def saveToCsv(fName, counts, topWords):
     with open(prefix + fName + ending, 'w') as csvfile:
         fieldnames = header
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
+        wordsSaved = 0 
         for key in counts: 
-            writer.writerow({header[0]: key, header[1]: counts[key]})
+            if(wordsSaved<topWords):
+                writer.writerow({header[0]: key, header[1]: counts[key]})
+                wordsSaved +=1 
+            else:
+                break 
         
-def createTitleSummaryFile(fName):
+def createTitleSummaryFile(fName, topWords):
     if os.path.isfile(prefix+fName+ending) == False:
         files = fName.split(fileNameSplit)
         wordList = []
@@ -49,5 +55,8 @@ def createTitleSummaryFile(fName):
             if(len(f)==2) and f[0] in allowedId and f[1] in allowedId:
                 wordList += loadTitles(f[0], f[1])
         counts = getDict(wordList)
-        saveToCsv(fName, counts)
+        saveToCsv(fName, counts, topWords)
+    return prefix + fName + ending
         
+        
+    
